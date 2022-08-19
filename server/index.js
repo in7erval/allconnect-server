ecb8ed849a546ed47c5d5f107713a5505c51cb21
Dmitry Yudakov <yudakov.d@mail.ru>
2022-08-20 00:50:09 +0300

    fix hotspot

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(jsonParser);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/uploads', express.static('uploads'));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://109.120.191.219:3000");
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
